eb7e0a9e3499174aab8ec3bd85a1237a7772331d
kristofdaniel25 <danokristof@gmail.com>
2020-11-30 23:05:31 +0100

    refaktoring

--- metar.sh
+++ metar.sh
@@ -11,7 +11,5 @@ fi
 MOJ_STARTTIME=$(date -d "$1" +'%s')
 MOJ_ENDTIME=$(echo "$MOJ_STARTTIME"+"24*60*60" | bc)
 
-
-echo start =  "$MOJ_STARTTIME" end = "$MOJ_ENDTIME"
-
+#prvych 6 riadkov some sa zbavil pomocou sed:
 curl -s 'https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=csv&stationString='"$2"'&startTime='"$MOJ_STARTTIME"'&endTime='"$MOJ_ENDTIME" | sed '1d;2d;3d;4d;5d;6d'
