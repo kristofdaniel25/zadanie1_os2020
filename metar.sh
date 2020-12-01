29719b41d6dde52149e5030a4c9e1b53d5f44e69
kristofdaniel25 <danokristof@gmail.com>
2020-12-01 12:26:37 +0100

    metar.sh upravene pre zadanie1

--- metar.sh
+++ metar.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
+POCET_ARG=0
+
 #kontrola poctu argumentov
-if [ "$#" != 2 ]
+if [ "$#" == 1 ] 
 then
-	echo 'error argumenty: nespravny pocet argumentov.'
+	MOJ_STARTTIME=$(date -d yesterday +'%s')
+	POCET_ARG=1
+elif [ "$#" == 2 ]
+
+then
+	MOJ_STARTTIME=$(date -d "$1" +'%s')
+	POCET_ARG=2
+elif [ "$#" > 2 ] 
+then
+	echo 'error argumenty: Nespravny pocet argumentov.'
 	exit 1
 fi
 
-#spracovanie datumu a priprava endTime
-MOJ_STARTTIME=$(date -d "$1" +'%s')
+echo pocet_arg = "$POCET_ARG"
+
+#priprava endTime
+
 MOJ_ENDTIME=$(echo "$MOJ_STARTTIME"+"24*60*60" | bc)
 
 #prvych 6 riadkov som sa zbavil pomocou sed:
-curl -s 'https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=csv&stationString='"$2"'&startTime='"$MOJ_STARTTIME"'&endTime='"$MOJ_ENDTIME" | sed '1d;2d;3d;4d;5d;6d'
+if [ "$POCET_ARG" = 1 ]
+then
+	curl -s 'https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=csv&stationString='"$1"'&startTime='"$MOJ_STARTTIME"'&endTime='"$MOJ_ENDTIME" | sed '1d;2d;3d;4d;5d;6d'
+elif [ "$POCET_ARG" = 2 ]
+then
+	curl -s 'https://aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&format=csv&stationString='"$2"'&startTime='"$MOJ_STARTTIME"'&endTime='"$MOJ_ENDTIME" | sed '1d;2d;3d;4d;5d;6d'
+fi
+
